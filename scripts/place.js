3dbc5c6efc41f674065660d098838e2d63f20dd0
Sergio Herrera Rivera <sh0041314@gmail.com>
2024-06-29 23:53:55 -0600

    completo

--- scripts/place.js
+++ scripts/place.js
@@ -1,8 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Set the current year
-    document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", () => {
+    const temp = document.querySelector("#temperature");
+    const condit = document.querySelector("#condition");
+    const wind = document.querySelector("#wind");
+    const windchill = document.querySelector("#wind-chill");
 
-    // Set the last modified date
+    const temperature = 27;  // Grados Celsius
+    const condition = "Cloudy";
+    const windSpeed = 9;  // km/h
+
+    function calculateWindChill(temperature, windSpeed) {
+        return (13.12 + (0.6215 * temperature) - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temperature) * Math.pow(windSpeed, 0.16)).toFixed(2);
+    }
+
+    temp.textContent = `${temperature} °C`;
+    condit.textContent = condition;
+    wind.textContent = `${windSpeed} km/h`;
+
+    if (temperature <= 10 && windSpeed > 4.8) {
+        windchill.textContent = `${calculateWindChill(temperature, windSpeed)} °C`;
+    } else {
+        windchill.textContent = "N/A";
+    }
+
+    
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
     document.getElementById("lastModified").textContent = document.lastModified;
 });
-
